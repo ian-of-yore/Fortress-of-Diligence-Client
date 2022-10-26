0159d664466df6ce84f5fbd7868f2fa89f5e64e9
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-27 01:23:37 +0600

    Card summary styling done

--- src/pages/Courses/CourseSummary/CourseSummary.js
+++ src/pages/Courses/CourseSummary/CourseSummary.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { FaChalkboardTeacher, FaRegClock } from "react-icons/fa";
+import { IoIosPeople } from "react-icons/io";
+import { GrUserWorker } from "react-icons/gr";
 
 const CourseSummary = ({ course }) => {
     // console.log(course)
 
-    const { title, author, course_id, details, image_url, info } = course;
+    const { title, author, course_id, image_url, info } = course;
 
     return (
         <div className='mb-5 shadow-lg'>
             <Link className='text-decoration-none' to={`/courses/${course_id}`}>
                 <Card text='white' className='bg-dark' style={{ height: "400px" }}>
                     <Card.Img variant="top" src={image_url} style={{ height: "250px" }} />
-                    <div className='d-flex justify-content-between align-items-center px-3 mt-1'>
-                        <p><small>{info.students} students</small></p>
-                        <p><small>{info.duration}</small></p>
+                    <div className='d-flex justify-content-between align-items-center px-3'>
+                        <p className='d-flex align-items-center'><small><IoIosPeople className='mb-1' style={{ height: "40px" }}></IoIosPeople> {info.students}</small></p>
+                        <p className='d-flex align-items-center'><small><FaRegClock className='mb-1' style={{ height: "40px" }}></FaRegClock> {info.duration}</small></p>
                     </div>
-                    <p className='fs-5'>{title}</p>
-                    <div className='d-flex justify-content-between align-items-center'>
-                        <p>{author.name}</p>
-                        <p><small>{author.profession}</small></p>
-                    </div>
+                    <Card.Title><p className='fs-5 px-3'>{title}</p></Card.Title>
+                    <Card.Footer className='d-flex justify-content-between align-items-center'>
+                        <p className='mb-0'><FaChalkboardTeacher className='me-1'></FaChalkboardTeacher> {author.name}</p>
+                        <p className='mb-0'><small><GrUserWorker className='rounded me-1' style={{ backgroundColor: "white", height: "16px", width: "16px" }}></GrUserWorker> {author.profession}</small></p>
+                    </Card.Footer>
                 </Card>
             </Link>
         </div>
