a66854b670d0efc0f07e77fcbacbf0ee8f4355b8
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-27 01:41:49 +0600

    course details page styling done

--- src/pages/Courses/CourseDetails/CourseDetails.js
+++ src/pages/Courses/CourseDetails/CourseDetails.js
@@ -3,6 +3,8 @@ import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useLoaderData, Link } from 'react-router-dom';
+import { MdOutlineSubtitles, MdAttachMoney } from "react-icons/md";
+import { GrDocumentDownload } from "react-icons/gr";
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
@@ -13,12 +15,12 @@ const CourseDetails = () => {
     return (
         <Container className='my-5'>
 
-            <Card className='text-start w-50 mx-auto border border-1 bg-dark text-white'>
-                <div className='d-flex justify-content-around align-items-center text-white px-4 shadow-lg py-4'>
-                    <h3>{title}</h3>
-                    <p className='ps-3'>icon</p>
+            <Card className='text-start w-75 mx-auto border border-1 bg-dark text-white'>
+                <div className='d-flex justify-content-between align-items-center px-5 text-white shadow-lg py-4'>
+                    <h4 className='text-left'><MdOutlineSubtitles className='pb-1' style={{ height: "30px" }}></MdOutlineSubtitles> {title}</h4>
+                    <p className='ps-3'><GrDocumentDownload style={{ backgroundColor: "White", height: "25px", width: "25px" }}></GrDocumentDownload></p>
                 </div>
-                <Card.Img variant="top" src={image_url} style={{ height: "400px" }} />
+                <Card.Img variant="top" src={image_url} style={{ height: "450px" }} />
                 <div className='d-flex justify-content-around mt-4  text-center'>
                     <div className='px-5 py-2 text-white shadow-lg rounded'>
                         <h5>{info.students}</h5>
@@ -40,7 +42,7 @@ const CourseDetails = () => {
                 <Card.Text className='mt-1 mb-3 mx-4 shadow-lg px-4 py-2'>
                     {details}
                 </Card.Text>
-                <Button variant="outline-secondary"><Link className='text-decoration-none text-white' to={`/courses/${course_id}/checkout`}>Get premium access</Link></Button>
+                <Button variant="outline-secondary"><Link className='text-decoration-none text-white' to={`/courses/${course_id}/checkout`}><MdAttachMoney></MdAttachMoney> Get premium access <MdAttachMoney></MdAttachMoney></Link></Button>
             </Card>
         </Container>
     );
--- src/pages/Courses/LeftSideNav/LeftSideNav.js
+++ src/pages/Courses/LeftSideNav/LeftSideNav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const LeftSideNav = ({ title, id }) => {
     return (
-        <div className='mb-4 shadow-lg p-2'>
-            <p><small><Link className='text-decoration-none text-white' to={`/courses/${id}`}>{title}</Link></small></p>
-        </div>
+        <Button variant='dark' className='mb-4 shadow-lg p-2 border'>
+            <Link className='text-decoration-none text-white' to={`/courses/${id}`}> <p><small>{title}</small></p></Link>
+        </Button>
     );
 };
 
